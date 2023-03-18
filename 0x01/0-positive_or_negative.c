524734fa46b182b750023e76da39b52a2f52cf60
AlexHolman10 <alexholman9991@gmail.com>
2023-03-18 02:38:45 +0200

    Task 0

---
+++ 0x01/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* 
+* main - This program will assign a random number to the variable n each time it is executed.
+* return 0 if program runs successfully.
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+	else if (n > 0)
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
+	return (0);
+}
